e61b45164f63bc541faa8964947a80e966c222f8
gerardlezcan <gerardlezcano@gmail.com>
2019-12-02 21:11:45 -0300

    Controlador de Doctor creado

---
+++ MediCsharpASP/Controllers/DoctorController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MediCsharpASP.Models;
+
+namespace MediCsharpASP.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private MediCsharp2Entities db = new MediCsharp2Entities();
+
+        // GET: Doctor
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Doctor.ToListAsync());
+        }
+
+        // GET: Doctor/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = await db.Doctor.FindAsync(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctor/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,NombreDoctor,ApellidoDoctor,especialidad,sexo,Edad,FechaNacimiento,Telefono")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Doctor.Add(doctor);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(doctor);
+        }
+
+        // GET: Doctor/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = await db.Doctor.FindAsync(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctor/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,NombreDoctor,ApellidoDoctor,especialidad,sexo,Edad,FechaNacimiento,Telefono")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctor).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctor/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = await db.Doctor.FindAsync(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Doctor doctor = await db.Doctor.FindAsync(id);
+            db.Doctor.Remove(doctor);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
